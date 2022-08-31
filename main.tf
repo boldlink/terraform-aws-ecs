45755c2635a8b325cf94cc018216cda1c0123367
patrick <mukumbu.katiwa@boldlink.io>
2022-08-31 13:18:45 +0300

    NOSTORY: modify access logs condition

--- main.tf
+++ main.tf
@@ -111,7 +111,7 @@ resource "aws_lb" "main" {
   enable_deletion_protection = var.enable_deletion_protection
 
   dynamic "access_logs" {
-    for_each = (var.load_balancer_type == "Application" || var.load_balancer_type == "Network") && length(keys(var.access_logs)) == 0 ? [] : [var.access_logs]
+    for_each = local.legible_lb_type && length(keys(var.access_logs)) != 0 ? [var.access_logs] : []
 
     content {
       bucket  = access_logs.value.bucket
