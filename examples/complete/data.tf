8060019c0c6c1457f77f78f87258c13ebe48374c
Hugo Almeida <hugo.almeida@boldlink.io>
2023-11-08 16:22:44 +0000

    fix: remove unused account_id datasource

--- examples/complete/data.tf
+++ examples/complete/data.tf
@@ -2,8 +2,6 @@ data "aws_partition" "current" {}
 
 data "aws_region" "current" {}
 
-data "aws_caller_identity" "current" {}
-
 data "aws_iam_policy_document" "ecs_assume_role_policy" {
   statement {
     actions = ["sts:AssumeRole"]
