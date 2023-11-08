3b9cd693850543da17a6ed14c304c9447e80335a
Hugo Almeida <hugo.almeida@boldlink.io>
2023-11-08 16:14:47 +0000

    nostory: fix outputs and add notes to readme complete example

--- examples/complete/outputs.tf
+++ examples/complete/outputs.tf
@@ -1,10 +1,10 @@
-output "alb_dns_name" {
-  value       = [module.ecs_service_alb.lb_dns_name]
+output "alb_service_url" {
+  value       = "https://${module.ecs_service_alb.lb_dns_name}/v1/api"
   description = "The task definition arn"
 }
 
-output "alb_arn" {
-  value       = [module.ecs_service_alb.lb_arn]
-  description = "The load balancer arn/id"
+output "nlb_service_url" {
+  value       = "https://${module.ecs_service_nlb.lb_dns_name}:5000/v1/api"
+  description = "The task definition arn"
 }
 
